755736cdcd7db694fc8fb26bd166176f9494a5d6
hikalium <hikalium@hikalium.com>
2024-10-17 00:03:46 +0900

    Impl Task and block_on for Future support
    Change-Id: I3e814f35266cb2c864e2c61dd2a36d3b21ce52b3

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use core::fmt::Write;
 use core::panic::PanicInfo;
 use core::writeln;
 use wasabi::error;
+use wasabi::executor::block_on;
 use wasabi::graphics::draw_test_pattern;
 use wasabi::graphics::fill_rect;
 use wasabi::graphics::Bitmap;
@@ -92,6 +93,12 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
             .expect("Failed to unmap page 0");
     }
     flush_tlb();
+
+    let result = block_on(async {
+        info!("Hello from the async world!");
+        Ok(())
+    });
+    info!("block_on completed! result = {result:?}");
 
     loop {
         hlt()
