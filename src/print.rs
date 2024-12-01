fed97b52a2e342d18f8fa249e2eb779c61e5c40e
hikalium <hikalium@hikalium.com>
2024-12-01 22:41:23 +0900

    to be refactored: usb hid mouse report desc
    Change-Id: I7ea1a4335ebd7d7f1c1a266cba488827425b3f36

--- src/print.rs
+++ src/print.rs
@@ -50,7 +50,7 @@ macro_rules! error {
                     file!(), line!(), format_args!($($arg)*)));
 }
 
-fn hexdump_bytes(bytes: &[u8]) {
+pub fn hexdump_bytes(bytes: &[u8]) {
     let mut i = 0;
     let mut ascii = [0u8; 16];
     let mut offset = 0;
@@ -101,7 +101,8 @@ fn hexdump_bytes(bytes: &[u8]) {
         println!("|");
     }
 }
-pub fn hexdump<T: Sized>(data: &T) {
+pub fn hexdump_struct<T: Sized>(data: &T) {
+    info!("hexdump_struct: {:?}", core::any::type_name::<T>());
     hexdump_bytes(unsafe {
         slice::from_raw_parts(data as *const T as *const u8, size_of::<T>())
     })
